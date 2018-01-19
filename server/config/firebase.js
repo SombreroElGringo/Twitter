719c26d1201cef031969636544bf5952fa49dea8
SombreroElGringo <florent.pailhes@hotmail.fr>
2018-01-19 22:12:47 +0100

    [Enhancement] Client & Server
    * config files with firebase keys

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,12 +1,18 @@
 const admin = require('firebase-admin');
 const chalk = require('chalk');
-let serviceAccount = require('./serviceAccountKey.json');
-/*
-serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT,
-    privateKey: process.env.FIREBASE_API_KEY
-};*/
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: JSON.parse(`"${process.env.FIREBASE_PRIVATE_KEY}"`),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 module.exports = function() {
     // Initialize Firebase
